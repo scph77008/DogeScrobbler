d3ad4d31ae0786b82a60ded81d10ed32377c1b0f
Андрей <andrey@openstart.ru>
2015-11-25 17:02:08 +0300

    JSON: Какой-никакой вывод

--- js/scrobbler.js
+++ js/scrobbler.js
@@ -1,35 +1,56 @@
 $(document).ready(function ($)
 {
-	const API_KEY = '2417947c57e2d0cde512441710dcb29f';
-	$('#submit').on('click', function ()
-	{
+	//$('#submit').on('click', function ()
+//		{
 		//alert('wow!');
 
-		var $user      = $('#user');
-		var $limit     = $('#limit');
-		var $date_from = $('#date');
+		var $user      = $('#user').val();
+		var $limit     = $('#limit').val();
+		var $date_from = new Date($('#date').val()).getTime() / 1000;
+		var $date_to   = parseInt($date_from + 60 * 60 * 24);
 
-		var $user_val      = $user.val();
-		var $limit_val     = $limit.val();
-		var $date_from_val = $date_from.val();
-		var $date_to_val = parseInt($date_from_val + 60 * 60 * 24);  //TODO: int
-
-		var $url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=' + $user_val +
-			'&limit=' + $limit_val +
-			'&from=' + $date_from_val +
-			'&to=' + $date_to_val +
-			'&api_key=2417947c57e2d0cde512441710dcb29f' +
-			'&format=json';
-
-		alert($date_from_val);
-		alert($date_to_val);
 		$.ajax({
-			url    : $url,
+			url    : 'http://ws.audioscrobbler.com/2.0/',
+			data   : {
+				method : 'user.getrecenttracks',
+				user   : $user,
+				limit  : $limit,
+				from   : $date_from,
+				to     : $date_to,
+				api_key: '2417947c57e2d0cde512441710dcb29f',
+				format : 'json'
+			},
 			success: function (data)
 			{
-				console.log(data);
+				var $album_image;
+				var $album_title;
+				var $track_artist;
+				var $track_title;
+				var $date;
+				var $dateUTC;
+				var $charts = $('.charts');
+				console.log(data.recenttracks);
+				$.each((data.recenttracks.track), function(id, track )
+				{
+					$album_image = track.image[2]['#text']; // large, 174x174px
+					$album_title = track.album['#text'];
+					$track_artist = track.artist['#text'];
+					$track_title = track.name;
+					$date = track.date['#text']; // example: "23 Nov 2015, 20:39"
+					$dateUTC = track.date.uts; // example: "1448311164"
+
+					$charts.append(
+						'<div class="track">' +
+							'<div class="album_logo"><img src=' + $album_image + '/></div>' +
+							'<div class="artist">' + $track_artist + '</div>' +
+							'<div class="track">' + $track_title + '</div>' +
+							'<div class="album">' + $album_title + '</div>' +
+							'<div class="date">' + $date + '</div>' +
+						'</div>');
+
+				});
+
 			}
 		});
-
-	});
+//	});
 });
